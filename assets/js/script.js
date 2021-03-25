635c70622f5d45596fa9b5311f140eebf91ead24
Emily Crook <Emily.A.Crook96@gmail.com>
2021-03-24 20:38:31 -0500

    addition of upcomming due date 'warnings' with color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
 var tasks = {};
 
+var auditTask = function(taskEl) {
+  // get date form task element
+  var date = $(taskEl).find("span").text().trim();
+
+  //convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  
+  // remove any old classes form element
+  $(taskEl).removeClass("list-group-item0warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -13,6 +32,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -103,7 +124,7 @@ $(".list-group").on("click", "span", function() {
 
   // enable jquery ui datepicker
   dateInput.datepicker({
-    minDate: 1
+    minDate: 1,
     onClose: function() {
       // When calendar is closed, force a "change" event on the 'dateInput'
       $(this).trigger("change");
@@ -143,6 +164,9 @@ $(".list-group").on("change", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 
@@ -262,3 +286,4 @@ $("#trash").droppable({
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
